86115adfc9619950383a736b63eee49911824cb4
João Victor Julião <107512421+joaovictojuliao@users.noreply.github.com>
2022-07-15 13:39:39 -0300

    Create Disemvowel Trolls.py

---
+++ Disemvowel Trolls.py
@@ -0,0 +1,18 @@
+def disemvowel(string_):
+    string2=""
+    for i in string_:
+        if i!="a":
+            if i!="e":
+                if i!="i":
+                    if i!="o":
+                        if i!="u":
+                             if i!="A":
+                                 if i!="E":
+                                     if i!="I":
+                                         if i!="O":
+                                             if i!="U":
+                                                string2=string2+i
+                            
+                            
+    string2==string_
+    return string2
